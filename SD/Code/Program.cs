f5bbf6619547c7f0f42b1c8dabade9515b0474a0
NekoDar <neko-d-05@mail.ru>
2024-03-16 13:16:21 +0300

    Оптимизация использования памяти

--- SD/Code/Program.cs
+++ SD/Code/Program.cs
@@ -1,16 +1,16 @@
 ﻿using SD.Code.Compiler;
 using SD.Code.Decompile;
-using System;
 
 class Program
 {
     static void Main()
     {
+        Console.ForegroundColor = ConsoleColor.Green;
+        string logo = "                      _ _           _                            _             \n  ___ _   _ _ __   __| (_)       __| | ___  ___ _ __ _   _ _ __ | |_ ___  _ __ \n / __| | | | '_ \\ / _` | |_____ / _` |/ _ \\/ __| '__| | | | '_ \\| __/ _ \\| '__|\n \\__ \\ |_| | | | | (_| | |_____| (_| |  __/ (__| |  | |_| | |_) | || (_) | |   \n |___/\\__, |_| |_|\\__,_|_|      \\__,_|\\___|\\___|_|   \\__, | .__/ \\__\\___/|_|   \n      |___/                                          |___/|_|                  \n";
+        
         while (true)
         {
             Console.Clear();
-            Console.ForegroundColor = ConsoleColor.Green;
-            string logo = "                      _ _           _                            _             \n  ___ _   _ _ __   __| (_)       __| | ___  ___ _ __ _   _ _ __ | |_ ___  _ __ \n / __| | | | '_ \\ / _` | |_____ / _` |/ _ \\/ __| '__| | | | '_ \\| __/ _ \\| '__|\n \\__ \\ |_| | | | | (_| | |_____| (_| |  __/ (__| |  | |_| | |_) | || (_) | |   \n |___/\\__, |_| |_|\\__,_|_|      \\__,_|\\___|\\___|_|   \\__, | .__/ \\__\\___/|_|   \n      |___/                                          |___/|_|                  \n";
             Console.WriteLine(logo);
             Console.WriteLine("1) Decompile build");
             Console.WriteLine("2) Compiler");
